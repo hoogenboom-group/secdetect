fdaf2e8f5957895708e7f01c00b3ad3f2d887c1a
lanery <ryanilane@gmail.com>
2019-03-27 14:02:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,22 +6,24 @@ MAINTAINER = 'Ryan Lane'
 MAINTAINER_EMAIL = 'r.i.lane@tudelft.nl'
 LICENSE = 'LICENSE.txt'
 URL = 'https://github.com/lanery/secdetect'
-VERSION = '0.1.0'
+VERSION = '0.1.dev'
 PACKAGES = ['secdetect']
 INSTALL_REQUIRES = [
     'numpy',
     'scikit-image',
 ]
 
-setup(
-    name=DISTNAME,
-    version=VERSION,
-    author=MAINTAINER,
-    author_email=MAINTAINER_EMAIL,
-    packages=PACKAGES,
-    url=URL,
-    license=LICENSE,
-    description=DESCRIPTION,
-    long_description=open('README.md').read(),
-    install_requires=INSTALL_REQUIRES,
-)
+if __name__ == '__main__':
+
+    setup(
+        name=DISTNAME,
+        version=VERSION,
+        author=MAINTAINER,
+        author_email=MAINTAINER_EMAIL,
+        packages=PACKAGES,
+        url=URL,
+        license=LICENSE,
+        description=DESCRIPTION,
+        long_description=open('README.md').read(),
+        install_requires=INSTALL_REQUIRES,
+    )
