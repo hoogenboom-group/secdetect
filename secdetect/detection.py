859b15a9f9e644ed6fd7ad12f3bc65013478b3ea
Ryan Lane <ryanilane@gmail.com>
2019-07-20 19:02:42 +0200

    Added blob analysis LoG

--- secdetect/detection.py
+++ secdetect/detection.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from skimage.feature import canny
+from skimage.feature import canny, blob_log
 from skimage.segmentation import felzenszwalb, clear_border
 from skimage.color import hed_from_rgb
 
@@ -10,31 +10,25 @@ from .ringdetect import find_ring, crop_to_ring
 __all__ = ['detect_sections']
 
 
-def detect_sections(image, enhance_contrast_kws=None,
-                    canny_kws=None, find_ring_kws=None,
-                    felzenszwalb_kws=None, clear_border_kws=None):
-    """Detect sections from optical overview image
+def remove_background(image, enhance_contrast_kws=None,
+                      canny_kws=None, find_ring_kws=None)
+    """Remove background from optical overview image
 
     Parameters
     ----------
-    image : ndarray
+    image : 2D (RGB or BW) array
         Input optical overview image
     enhance_contrast_kws : dict (optional)
         Keyword arguments for `enhance_contrast`
     canny_kws : dict (optional)
-        Keyword arguments for `canny`
+        Keyword arguments for Canny edge detector
     find_ring_kws : dict (optional)
         Keyword arguments for `find_ring`
-    felzenszwalb_kws : dict (optional)
-        Keyword arguments for `felzenswalb`
-    clear_border_kws : dict (optional)
-        Keyword arguments for `clear_border`
 
     Returns
     -------
-    imcb : ndarray
-        Segmented image in which the intensity levels correspond to unique,
-        detected sections
+    imcr : 2D (RGB or BW) array
+        Input image with removed background
     """
     # Make unnecessary copy of input image to preserve it
     imin = image.copy()
@@ -60,6 +54,38 @@ def detect_sections(image, enhance_contrast_kws=None,
         find_ring_kws = {}
     find_ring_kws.setdefault('ransac_kws', None)
 
+    # Enhance contrast
+    imec = enhance_contrast(imin, **enhance_contrast_kws)
+    # Canny edge detector for ring detection
+    imcn = canny(imec, **canny_kws)
+    # Find ring from edges and extract paramters (center coords, radius)
+    cx, cy, r = find_ring(imcn, **find_ring_kws)
+    # Crop to ring and remove background
+    imcr = crop_to_ring(imin, cx=cx, cy=cy, radius=r)
+    return imcr
+
+
+def detect_sections_rough(image, felzenszwalb_kws=None, clear_border_kws=None):
+    """Detect sections using Felzenszwalb segmentation
+
+    Parameters
+    ----------
+    image : 2D (RGB or BW) array
+        Input optical overview image
+    felzenszwalb_kws : dict (optional)
+        Keyword arguments for Felzenswalb segmentation
+    clear_border_kws : dict (optional)
+        Keyword arguments for `clear_border`
+
+    Returns
+    -------
+    imcb : 2D (RGB or BW) array
+        Segmented image in which the intensity levels correspond to unique,
+        detected sections
+    """
+    # Make unnecessary copy of input image to preserve it
+    imin = image.copy()
+
     # Set default parameters for `felzenswalb`
     if felzenszwalb_kws is None:
         felzenszwalb_kws = {}
@@ -73,17 +99,48 @@ def detect_sections(image, enhance_contrast_kws=None,
         clear_border_kws = {}
     clear_border_kws.setdefault('buffer_size', 50)
 
-    # Enhance contrast
-    imec = enhance_contrast(imin, **enhance_contrast_kws)
-    # Canny edge detector for ring detection
-    imcn = canny(imec, **canny_kws)
-    # Find ring from edges and extract paramters (center coords, radius)
-    cx, cy, r = find_ring(imcn, **find_ring_kws)
-    # Crop to ring and remove background
-    imcr = crop_to_ring(imin, cx=cx, cy=cy, radius=r)
+    # Remove background
+    imcr = remove_background(imin)
     # Felzenswalb segmentation
     imfz = felzenszwalb(imcr, **felzenszwalb_kws)
     # Clear segments around border
     imcb = clear_border(imfz, **clear_border_kws)
-
     return imcb
+
+def detect_sections_blob(image, blob_log_kws=None):
+    """Detect sections using Laplacian of Gaussian blob analysis
+
+    Parameters
+    ----------
+    image : 2D (RGB or BW) array
+        Input optical overview image
+    blob_log_kws : dict
+        Keyword arguments for `blob_log`
+
+    Returns
+    -------
+    blobs : 2D BW array
+        Detected blobs
+    """
+    # Make unnecessary copy of input image to preserve it
+    imin = image.copy()
+
+    # Set default parameters for ``
+    if blob_log_kws is None:
+        blob_log_kws = {}
+    blob_log_kws.setdefault('min_sigma', 8)
+    blob_log_kws.setdefault('max_sigma', 12)
+    blob_log_kws.setdefault('num_sigma', 3)
+    blob_log_kws.setdefault('threshold', 0.1)
+    blob_log_kws.setdefault('overlap', 0.2)
+
+    # Set default parameters for `clear_border`
+    if clear_border_kws is None:
+        clear_border_kws = {}
+    clear_border_kws.setdefault('buffer_size', 50)
+
+    # Remove background
+    imcr = remove_background(imin)
+    # Blob analysis
+    blobs = blob_log(imcr, **blob_log_kws)
+    return blobs
