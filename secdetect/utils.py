085d298a9ee5f1bbf40cfa92bc0af099e6422998
lanery <ryanilane@gmail.com>
2021-03-01 15:42:05 +0100

    added merge_segments function

--- secdetect/utils.py
+++ secdetect/utils.py
@@ -2,7 +2,57 @@ import numpy as np
 from matplotlib.colors import LinearSegmentedColormap
 
 
-__all__ = ['neon_cmap']
+__all__ = ['neon_cmap',
+           'merge_segments']
+
+
+def merge_segments(image, segments_2_merge, label=None):
+    """Merge segments with fancy numpy indexing
+
+    Parameters
+    ----------
+    image : array-like
+        Segmented image with labels as intensity values e.g.
+        [[0, 0, 1, 0],
+         [2, 0, 1, 1],
+         [2, 0, 1, 0],
+         [0, 3, 0, 0]]
+
+    segments_2_merge: list-like
+        List of segments to merge
+
+    label : scalar
+        Label to give newly merged segment
+        Defaults to lowest valued label provided
+
+    Returns
+    -------
+    segments_map : array-like
+        Image with merged segments
+
+    Notes
+    -----
+    * Makes use of the numpy trick
+    >>> a = np.array([[0, 1, 2],
+                      [2, 1, 0],
+                      [0, 1, 2]])
+    >>> mapping = np.array([-5, 2**3, 17])
+    >>> mapping[a]
+    array([[-5,  8, 17],
+           [17,  8, -5],
+           [-5,  8, 17]])
+
+    References
+    ----------
+    [1] https://stackoverflow.com/a/57916846/5285918
+    """
+    # Set label if not provided
+    if label is None:
+        label = min(segments_2_merge)
+    segments_map = np.arange(np.max(image) + 1)
+    segments_map[segments_2_merge] = label
+    # Tricky index remapping
+    return segments_map[image]
 
 
 def neon_cmap():
