005eb147c8740ea599a88b39c5e64fd7c29fee3a
lanery <ryanilane@gmail.com>
2021-03-01 13:24:54 +0100

    create utils module for neon colormap

---
+++ secdetect/utils.py
@@ -0,0 +1,20 @@
+import numpy
+from matplotlib.colors import LinearSegmentedColormap
+
+
+__all__ = ['neon_cmap']
+
+
+def neon_cmap():
+    """Create a colormap with super bright colors above a black background"""
+    # Create neon colormap
+    base_colors = ['#000000',  # black
+                   '#ff99aa',  # pink
+                   '#ffcc00',  # orange
+                   '#ffff00',  # yellow
+                   '#00ff00',  # green
+                   '#00ddff']  # cyan
+    weights = [0] + np.linspace(0, 1, len(base_colors)-1).tolist()
+    weighted_colors = list(zip(weights, base_colors))
+    neon = LinearSegmentedColormap.from_list("", weighted_colors)
+    return neon
